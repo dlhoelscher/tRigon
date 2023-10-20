58b840f28ee9b220b51602305d64d5d256ac92ce
dlhoelscher <101323262+dlhoelscher@users.noreply.github.com>
2023-10-20 15:59:53 +0200

    bug fix fi

--- inst/app/ui-settings-page.R
+++ inst/app/ui-settings-page.R
@@ -4,16 +4,15 @@ settings_page <- fluidRow(
     box(
       width = NULL, title = "Settings",
       div(style = "margin-top: -10px;"),
-      tags$h4("Processing Mode (mouse / human):"),
+      tags$h4("Processing Mode (animal / human):"),
       materialSwitch(inputId = "workflowHuman", label = "Human data workflow"),
-      "If you want to analyse human data, select the toggle to change the processing mode - mouse data is enabled as default", br(),
-      "mouse data: experiment data with supplied metadata file including an ID (as given in the feature files), position (the slide position on the WSI) and group (WT vs MT, etc.) column", br(),
-      "human data: clinical data with the supplied metadata file including an ID (as given in the feature files), pseudonym (the sample identifier) and possible group (clinical data, diseases, etc.) column", br(),
-      "The processing workflows differ based on the way FLASH processes whole-slide images from mouse or human data.", br(), br(),
+      "If you want to analyse human data, select the toggle to change the processing mode - animal experiment data is enabled as default.",
+      "The processing workflows differ based on the way FLASH processes whole-slide images from animal experiment or human data.",
+      "For more information on how to structure the meta data for human or animal experiment data please visit the Help page within tRigon.", br(), br(),
       tags$h4("Keep feature calculations:"),
       materialSwitch(inputId = "keepCalculations", label = "Keep specimen level calculations"),
-      "If you want to analyse features on specimen level it is best to do this with feature medians or means - as default they are excluded from the data.", br(),
-      "By enabling to keep these specimen-level calculations each feature will come with specimen-level median, mean and standard deviation after processing.", br(),
+      "If you want to analyse features on specimen level it is best to do this with feature medians or means - as default they are excluded from the data.",
+      "By enabling to keep these specimen-level calculations each feature will come with specimen-level median, mean and standard deviation after processing.",
       "Please note that enabling this will only work during processing and not after pathomics data was already processed."
     )
   )
